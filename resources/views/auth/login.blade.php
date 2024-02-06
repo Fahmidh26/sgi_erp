dacff325ff2ac317c9e7523dd4dd5e9d76f52118
ifazalam1 <ifaz.alam1@gmail.com>
2024-02-06 12:39:53 +0600

    login page updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,10 +23,10 @@
                     <div class="text-center mt-sm-5 mb-4 text-white-50">
                         <div>
                             <a href="index" class="d-inline-block auth-logo">
-                                <img src="{{ URL::asset('assets/images/logo-light.png')}}" alt="" height="20">
+                                <img src="{{ URL::asset('assets/images/logo-light.png')}}" alt="" height="100">
                             </a>
                         </div>
-                        <p class="mt-3 fs-15 fw-medium">Premium Admin & Dashboard Template</p>
+                        {{-- <p class="mt-3 fs-15 fw-medium">Premium Admin & Dashboard Template</p> --}}
                     </div>
                 </div>
             </div>
@@ -39,7 +39,7 @@
                         <div class="card-body p-4">
                             <div class="text-center mt-2">
                                 <h5 class="text-primary">Welcome Back !</h5>
-                                <p class="text-muted">Sign in to continue to Velzon.</p>
+                                <p class="text-muted">Sign in to continue to SGI ERP.</p>
                             </div>
                             <div class="p-2 mt-4">
                                 <form action="{{ route('login') }}" method="POST">
@@ -88,10 +88,10 @@
                         <!-- end card body -->
                     </div>
                     <!-- end card -->
-
+{{-- 
                     <div class="mt-4 text-center">
                         <p class="mb-0">Don't have an account ? <a href="register" class="fw-semibold text-primary text-decoration-underline"> Signup </a> </p>
-                    </div>
+                    </div> --}}
 
                 </div>
             </div>
@@ -107,7 +107,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="text-center">
-                        <p class="mb-0 text-muted">&copy; <script>document.write(new Date().getFullYear())</script> Velzon. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand</p>
+                        <p class="mb-0 text-muted">&copy; <script>document.write(new Date().getFullYear())</script> STATA IT. Crafted with <i class="mdi mdi-heart text-danger"></i> by STATA IT</p>
                     </div>
                 </div>
             </div>
